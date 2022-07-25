d746517f3cac7b61c40eb79e8a3f2ab4c8b320db
jeniferSamper <jenifersamperusb@gmail.com>
2022-07-25 15:24:37 -0500

    mostrar imagenes y nombres

--- src/data.js
+++ src/data.js
@@ -1,9 +1,12 @@
-// estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+export const showImage = (element) => {
+   let imageCharacter = Object.values(element);
+   let arrayImage = imageCharacter[1].map(total => total.image);
+  return arrayImage;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export const showName = (element) => {
+  let nameCharacter = Object.values(element);
+  let arrayName = nameCharacter[1].map(total => total.name);
+  return arrayName;
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import {showImage, showName} from './data.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+let dataRyM = data;
+let showImages = showImage(dataRyM);
+let showNames = showName(dataRyM);
+let addImage, addName;
+
+for (let i=0; i < showImages.length; i++ ){
+    //let showLinks = document.getElementById("character"); no es necesario el img en html
+     let showLinks = document.createElement("img");
+    showLinks.src = showImages[i];
+    showLinks.id = "img"+i; //agregar id a la imagen
+    console.log("mostrar links", showLinks)
+    addImage = document.getElementById("div1");
+    addImage.appendChild(showLinks);
+    
+   // let showNamesCharacter = document.getElementById("nameCharacter"); no es necesario el label en html
+    let showNamesCharacter = document.createElement("label");
+    showNamesCharacter.textContent = showNames[i];
+    showNamesCharacter.id = "name"+i; //se puede hacer asi o llamando el id que ya trae la base de dado
+    console.log("prueba nombre", showNamesCharacter)
+    addName = document.getElementById("div1");
+    addName.appendChild(showNamesCharacter);
+    }
+
+
