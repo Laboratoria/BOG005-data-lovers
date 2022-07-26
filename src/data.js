399b0d2c34b67ba41f3338c9678e8e07e1c539ca
jeniferSamper <jenifersamperusb@gmail.com>
2022-07-26 10:36:18 -0500

    muestra imagen y nombre

--- src/data.js
+++ src/data.js
@@ -1,12 +1,12 @@
 
 export const showImage = (element) => {
    let imageCharacter = Object.values(element);
-   let arrayImage = imageCharacter[1].map(total => total.image);
-  return arrayImage;
+   let listImage = imageCharacter[1].map(total => total.image);
+  return listImage;
 };
 
 export const showName = (element) => {
   let nameCharacter = Object.values(element);
-  let arrayName = nameCharacter[1].map(total => total.name);
-  return arrayName;
+  let listName = nameCharacter[1].map(total => total.name);
+  return listName;
 }
--- src/main.js
+++ src/main.js
@@ -7,21 +7,17 @@ let showNames = showName(dataRyM);
 let addImage, addName;
 
 for (let i=0; i < showImages.length; i++ ){
-    //let showLinks = document.getElementById("character"); no es necesario el img en html
      let showLinks = document.createElement("img");
     showLinks.src = showImages[i];
-    showLinks.id = "img"+i; //agregar id a la imagen
-    console.log("mostrar links", showLinks)
-    addImage = document.getElementById("div1");
+    showLinks.id = "img"+i; 
+    addImage = document.getElementById("screen1");
     addImage.appendChild(showLinks);
     
-   // let showNamesCharacter = document.getElementById("nameCharacter"); no es necesario el label en html
-    let showNamesCharacter = document.createElement("label");
+   
+    let showNamesCharacter = document.createElement("span");
     showNamesCharacter.textContent = showNames[i];
-    showNamesCharacter.id = "name"+i; //se puede hacer asi o llamando el id que ya trae la base de dado
-    console.log("prueba nombre", showNamesCharacter)
-    addName = document.getElementById("div1");
+    showNamesCharacter.id = "name"+i; 
+    addName = document.getElementById("screen1");
     addName.appendChild(showNamesCharacter);
     }
-
 
