62451bcaa177e3f4cfb84f835d306601361da107
KarenthCruz <spoonecologic@gmail.com>
2022-08-02 10:42:25 -0500

    Prueba

--- src/main.js
+++ src/main.js
@@ -3,3 +3,10 @@ import { example } from './data.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+const nameMovie = document.getElementById("moveOne");
+const dataTitle = data.films[0];
+const titleMovie = dataTitle.title;
+nameMovie.innerHTML = '<li>'+ titleMovie + '</li>';
+
+console.log (titleMovie);
+
