011d862cc7f7e57a57d44edb2e33838e48ee2faf
jeniferSamper <jenifersamperusb@gmail.com>
2022-08-02 15:31:05 -0500

    intento de version responsive

--- src/data.js
+++ src/data.js
@@ -2,9 +2,8 @@
 export const showImage = (element) => {
    let imageCharacter = Object.values(element);
    let listImage = imageCharacter[1].map(total => total.image);
-     return listImage;
+  return listImage;
 };
-
 
 export const showName = (element) => {
   let nameCharacter = Object.values(element);
