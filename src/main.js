c2f307afdcf0716f56c2c58c0cefdf03dcb3c400
MODAOZUSH <yeimmy.lopez272@gmail.com>
2022-08-02 09:12:54 -0500

    Creacion funcion para mostrar listado de peliculas

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/ghibli/ghibli.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+// import { example } from './data.js';
 
+import data from './data/ghibli/ghibli.js';
+
+
+// data.films.forEach(function (film) {
+//     console.log(film.title)
+//     const ol = document.getElementById("peliculas");
+//     const li = document.createElement("li");
+//     const text = document.createTextNode(film.title);
+//     ol.appendChild(li)
+//     li.appendChild(text)
+// })
+
+const addFilmOnMovies = function(film) {
+    const ol = document.getElementById("peliculas");
+    const li = document.createElement("li");
+    const text = document.createTextNode(film.title);
+    ol.appendChild(li)
+    li.appendChild(text)
+};
+data.films.forEach(addFilmOnMovies)   
