a6f0391c30ed4dcd62bcd409d0291d5cce8a3e8e
AlexaFuenmayor <estherf.m18@gmail.com>
2022-08-02 13:07:23 -0500

    modifiqué html: quitando la lista y colocando la estrucutra para una tabla. en data.js cree función para filtrar datos del archivo js

--- src/data.js
+++ src/data.js
@@ -1,9 +1,19 @@
-// estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+export const filterData = (data, tipofiltro, valor) => {
+  //console.log(data);
+
+  let filtrado = [];
+
+  data.athletes.forEach(atleta => {
+    if(atleta[tipofiltro] == valor){
+      //console.log(atleta);
+      filtrado.push(atleta);
+    }
+  });
+
+    return filtrado;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+//export const data = () => {
+  //return 'data';
+//};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-import { example } from './data.js';
+import { filterData } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/athletes/athletes.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+
+
+let button = document.getElementById("buttonSearch");
+document.getElementById('listCard').style.visibility="hidden";
+
+
+function showList(){
+    document.getElementById('listCard').style.visibility="visible";
+}
+
+
+button.addEventListener("click", showList())
+console.log(filterData(data, "sport", "Taekwondo"));
+
+
+
