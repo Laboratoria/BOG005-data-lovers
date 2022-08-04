90b7c70efdf60ee95bd585936844812b0c8bcb90
Amappola <anam.porrasp@gmail.com>
2022-08-04 10:21:26 -0500

    Modificaciones no importantes

--- src/main.mjs
+++ src/main.mjs
@@ -1,6 +1,9 @@
 import allData from './allData.mjs';
-import htmlElements from './displayMain.mjs';
+// import htmlElements from './displayMain.mjs';
 
 // allData.getData()
 allData.getOrderedCharactersList()
 
+
+
+// Pagination
