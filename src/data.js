c7ab8dd89230b0e94dfa5c85fa8869957d883154
stefaniacamargo <estefaniacamargo263@gmail.com>
2022-08-04 11:50:35 -0500

    prueba2

--- src/data.js
+++ src/data.js
@@ -1,9 +1,19 @@
-// estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+//filter characters for houses
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export const functionGryffindor = (data) => {
+    return data.characters.filter(character => character.house && character.house.includes('Gryffindor'));
+}
+
+export const functionSlytterin = (data) => {
+    return data.characters.filter(character => character.house && character.house.includes('Slytterin'));
+}
+
+export const functionRavenclaw = (data) => {
+    return data.characters.filter(character => character.house && character.house.includes('Ravenclaw'));
+}
+
+export const functionHufflepuff = (data) => {
+    return data.characters.filter(character => character.house && character.house.includes('Hufflepuff'));
+}
+
