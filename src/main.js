940df2a52b203a7ccfda9689b2d670554262443d
Tania1295 <108893226+Tania1295@users.noreply.github.com>
2022-08-05 13:33:02 -0500

    Cambios en el main con const y recorrer el arreglo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { example } from './data.js';
 import data from './data/harrypotter/data.js';
 
-console.log(example, data);
+console.log(example, data.characters);
+
+//const personajes = data.characters
+
+//personajes.forEach(personaje => console.log(personaje))
+
+//const harrypotter = data.characters.find(harryPotter => harryPotter.name === "Harry Potter");
+//const ron = data.characters.find(ron => ron.name === "Ronald Weasley");
+//console.log (harrypotter, ron)
+
+for(const [key, value] of Object.entries(data.characters)){
+    console.log(value)
+}
