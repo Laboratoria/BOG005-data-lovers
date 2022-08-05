63815feb85b9cec96c6fb016ed8e1d2825732222
Amappola <anam.porrasp@gmail.com>
2022-08-05 09:10:37 -0500

    allData.mjs - Solucionando bug con el condicional switch

--- src/js_modules/allData.mjs
+++ src/js_modules/allData.mjs
@@ -1,15 +1,12 @@
 // allData.mjs - Extract characters information to process it
-
-import data from '../data/harrypotter/data.mjs';
-
 class ProcessData {
   constructor (data) {
-    // Importando personajes y libros del archivo data.js
+    // Importing characters and books from the dataset file
     this.characters = data.characters;
     this.books = data.books;
   }
 
-  // Función para crear una lista con el nombre de los personajes ordenados por apariciones en los libros
+  // Creating the list of characters ordered by the number of books they appear in
   getOrderedNamesList() {
     let importanceLevel1 = [];
     let importanceLevel2 = [];
@@ -30,19 +27,19 @@ class ProcessData {
 
       switch (characterBooks.length) {
         case 7:
-          characterName.push(importanceLevel1);
+          importanceLevel1.push(characterName);
           break;
         case 6:
-          characterName.push(importanceLevel2);
+          importanceLevel2.push(characterName);
           break;
         case 5:
-          characterName.push(importanceLevel3);
+          importanceLevel3.push(characterName);
           break;
         case 4:
-          characterName.push(importanceLevel4);
+          importanceLevel4.push(characterName);
           break;
         default:
-          characterName.push(importanceLevel5);
+          importanceLevel5.push(characterName);
           break;
       }
     });
