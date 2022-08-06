e253ac0323d41cdeabc15f05ccaf86d832c7a09c
AlexaFuenmayor <estherf.m18@gmail.com>
2022-08-05 22:11:43 -0500

    EN java Scripr cree función para visualizar la data en el select de paises, y agregué a la funcion de mostrar la data en pantalla: flag, limite, y contenedor seguido de unos if para crear divs de acuerdo a uno numero determinado de names- En html cambie table por secction para en vez de agregar filas y columnas  añadir unos div con inner

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+
 import { filterData } from "./data.js";
 import athletes from "./data/athletes/athletes.js";
 // import data from './data/lol/lol.js';
@@ -11,26 +12,66 @@ document.getElementById("page2").style.display= "none";
 
 let table = document.getElementById("contenedorTarjetas");
 document.getElementById("contenedorTarjetas").style.visibility = "visible";
+console.log(data)
+function showOptionsSelect (){
+  let team = [];
+  let selectPais = document.getElementById("selectBusquedaPais");
+    data.athletes.forEach(atleta =>{
+      if(team.indexOf(atleta["team"]) == -1){
+          team.push(atleta["team"]);
+      }
+      
 
+});
+  team.forEach(element => {
+    let paises = document.createElement("option");
+    paises.text = element;
+    paises.value = element;
+    selectPais.appendChild(paises);
+  });
 
+} showOptionsSelect()
 function showSport() {
  let dataSearch = document.getElementById("txtAtleta").value;
+ let firstMayus = dataSearch[0].toUpperCase()
+ let otherLetters = dataSearch.slice(1)
+ let typeMedal = firstMayus + otherLetters;
 
   table.innerHTML = "";
 
-  let result = filterData(data, "sport", dataSearch);
+  let result = filterData(data, "medal", typeMedal );
   console.log(result);
   let encabezado = document.createElement("td");
   encabezado.style.textAlign = "center";
-  encabezado.innerHTML = `ATLETAS EN ${dataSearch + " " + result.length}`;
+  encabezado.innerHTML = `Athletes who won  ${typeMedal + " medal "+" : "+ result.length}`;
+ //console.log(result.splice(10))
   table.appendChild(encabezado);
 
-  result.forEach((element) => {
-    let fila = document.createElement("tr"); //se crea la fial
-    fila.innerHTML = element.name; //se le agrega el contenido a la fila(nombre atletas)
-    table.appendChild(fila); //se inserta la fila en la tabla
-   
-  });
+
+    let flag = 0;
+    let limit = 25;
+    let contenedor;
+    result.forEach((element) => {
+      if(flag == limit){
+        flag = 0;
+      }
+      if(flag == 0){
+        contenedor = document.createElement("div");
+        contenedor.setAttribute("name", "helloButton");
+        contenedor.style.display = "inline-block";
+        contenedor.style.margin = "10px";
+
+      }
+      let fila = document.createElement("div");
+      let colum = document.createElement("td");
+      colum.innerHTML = element.name;
+       //se crea la fial
+      fila.appendChild(colum); //se le agrega el contenido a la fila(nombre atletas)
+      contenedor.appendChild(fila);
+      table.appendChild(contenedor); //se inserta la fila en la tabla
+     // table.appendChild(fila); //se inserta la fila en la tabla
+      flag++;
+    });
 
 }
 
