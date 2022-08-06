fc3d06d3a1eb6269165687a1980784bd4ccc9e74
Amappola <anam.porrasp@gmail.com>
2022-08-06 05:52:56 -0500

    Visualización de lista de personajes, falta corregir el tema de las imágenes

--- src/data/harrypotter/data.mjs
+++ src/data/harrypotter/data.mjs
@@ -1,30 +1,4 @@
 export default {
-  images: [
-    {
-      "specie": "Human",
-      "src": "http://imageshack.com/f/pnUFd2QWp"
-    },
-    {
-      "specie": "Centaur",
-      "src": "http://imageshack.com/f/po6UMYRmp"
-    },
-    {
-      "specie": "Peacock",
-      "src": "http://imageshack.com/f/pnz5obO0p"
-    },
-    {
-      "specie": "Rabbit",
-      "src": "http://imageshack.com/f/pn8crSSop"
-    },
-    {
-      "specie": "Acromantula",
-      "src": "http://imageshack.com/f/poTCxY5Sp"
-    },
-    {
-      "specie": "Generic",
-      "src": "http://imageshack.com/f/pmDlbIROp"
-    },
-  ],
   characters: [
     {
       "id": 1,
---
+++ src/js_modules/images.mjs
@@ -0,0 +1,26 @@
+export default [
+    {
+      "specie": "Human",
+      "src": "http://imageshack.com/f/pnUFd2QWp"
+    },
+    {
+      "specie": "Centaur",
+      "src": "http://imageshack.com/f/po6UMYRmp"
+    },
+    {
+      "specie": "Peacock",
+      "src": "http://imageshack.com/f/pnz5obO0p"
+    },
+    {
+      "specie": "Rabbit",
+      "src": "http://imageshack.com/f/pn8crSSop"
+    },
+    {
+      "specie": "Acromantula",
+      "src": "http://imageshack.com/f/poTCxY5Sp"
+    },
+    {
+      "specie": "Generic",
+      "src": "http://imageshack.com/f/pmDlbIROp"
+    },
+  ]
