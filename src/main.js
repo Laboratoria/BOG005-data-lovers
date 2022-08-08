f9248c990681c2e356c371942be6203784127710
AlexaFuenmayor <estherf.m18@gmail.com>
2022-08-08 11:35:08 -0500

    Creamos función para visualizar los atletas por deporte

--- src/main.js
+++ src/main.js
@@ -13,6 +13,9 @@ document.getElementById("page2").style.display= "none";
 let table = document.getElementById("contenedorTarjetas");
 document.getElementById("contenedorTarjetas").style.visibility = "visible";
 console.log(data)
+
+
+//crear opciones para seleccionar el país
 function showOptionsSelect (){
   let team = [];
   let selectPais = document.getElementById("selectBusquedaPais");
@@ -20,7 +23,6 @@ function showOptionsSelect (){
       if(team.indexOf(atleta["team"]) == -1){
           team.push(atleta["team"]);
       }
-      
 
 });
   team.forEach(element => {
@@ -29,24 +31,22 @@ function showOptionsSelect (){
     paises.value = element;
     selectPais.appendChild(paises);
   });
+} showOptionsSelect();
 
-} showOptionsSelect()
-function showSport() {
+
+function showAthletesByMedal() {
  let dataSearch = document.getElementById("txtAtleta").value;
  let firstMayus = dataSearch[0].toUpperCase()
  let otherLetters = dataSearch.slice(1)
  let typeMedal = firstMayus + otherLetters;
-
+ 
   table.innerHTML = "";
-
   let result = filterData(data, "medal", typeMedal );
   console.log(result);
   let encabezado = document.createElement("td");
   encabezado.style.textAlign = "center";
   encabezado.innerHTML = `Athletes who won  ${typeMedal + " medal "+" : "+ result.length}`;
- //console.log(result.splice(10))
   table.appendChild(encabezado);
-
 
     let flag = 0;
     let limit = 25;
@@ -65,17 +65,60 @@ function showSport() {
       let fila = document.createElement("div");
       let colum = document.createElement("td");
       colum.innerHTML = element.name;
-       //se crea la fial
-      fila.appendChild(colum); //se le agrega el contenido a la fila(nombre atletas)
+      fila.appendChild(colum); 
       contenedor.appendChild(fila);
-      table.appendChild(contenedor); //se inserta la fila en la tabla
-     // table.appendChild(fila); //se inserta la fila en la tabla
+      table.appendChild(contenedor); 
       flag++;
     });
+};
 
-}
+function showAthletesBySport(){
 
-document.getElementById("btnBuscar").addEventListener("click", showSport)
+  let selectSport = document.getElementById("selectBusquedaDeporte").value;
+  let resultSport= filterData(data, "sport", selectSport );
+  console.log(resultSport)
+  let flag = 0;
+  let limit = 25;
+  let contenedor;
+  resultSport.forEach((element) => {
+    if(flag == limit){
+      flag = 0;
+    }
+    if(flag == 0){
+      contenedor = document.createElement("div");
+      contenedor.setAttribute("name", "helloButton");
+      contenedor.style.display = "inline-block";
+      contenedor.style.margin = "10px";
+
+    }
+    let fila = document.createElement("div");
+    let colum = document.createElement("td");
+    colum.innerHTML = element.name;
+    fila.appendChild(colum); 
+    contenedor.appendChild(fila);
+    table.appendChild(contenedor); 
+    flag++;
+  });
+}  
+
+
+//SHOW OPTION DEPORTE
+function showOptionsSelectSport(){
+  let sport = [];
+  let selectSport = document.getElementById("selectBusquedaDeporte");
+    data.athletes.forEach(deporte =>{
+      if(sport.indexOf(deporte["sport"]) == -1){
+          sport.push(deporte["sport"]);
+      }
+});
+  sport.forEach(element => {
+    let sports = document.createElement("option");
+    sports.text = element;
+    sports.value = element;
+    selectSport.appendChild(sports);
+  });
+};
+showOptionsSelectSport ()
 
 
 //CAMBIAR DE PANTALLA
@@ -85,7 +128,6 @@ function shangePage() {
     document.getElementById("page1").style.display = "none";
     document.getElementById("page2").style.display = "inline";
 }
-btn1.addEventListener("click", shangePage);
 
 
 let btnInicio = document.getElementById("btnInicio");
@@ -93,4 +135,8 @@ function backPage1(){
     document.getElementById("page1").style.display = "inline";
     document.getElementById("page2").style.display = "none";
 }
+
+btn1.addEventListener("click", shangePage);
 btnInicio.addEventListener("click", backPage1)
+document.getElementById("btnBuscar").addEventListener("click", showAthletesByMedal, showAthletesBySport )
+;
