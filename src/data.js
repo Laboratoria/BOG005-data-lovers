2355c17be6947c43222ffea86ce93e7cf94441a1
AlexaFuenmayor <estherf.m18@gmail.com>
2022-08-09 12:12:46 -0500

    creamos función para mostrar atletas por paises

--- src/data.js
+++ src/data.js
@@ -14,6 +14,20 @@ export const filterData = (data, tipofiltro, valor) => {
     return filtrado;
 };
 
+export const filterDatas = (data, tipofiltro, tipofiltro2, valor1, valor2) => {
+  // console.log(valor);
+ 
+   let filtrado = [];
+ 
+   data.athletes.forEach(atleta => {
+     if(atleta[tipofiltro] == valor1 && [tipofiltro2] == valor2  ){
+       //console.log(atleta);
+       filtrado.push(atleta);
+     }
+   });
+ 
+     return filtrado;
+ };
 export const data = () => {
   return 'data';
 };
--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import athletes from "./data/athletes/athletes.js";
 import data from "./data/athletes/athletes.js";
 // import data from './data/rickandmorty/rickandmorty.js';
 
-let button = document.getElementById("buttonSearch");
+let buttonRefresh = document.getElementById("btnLimpiar");
 document.getElementById("contenedorTarjetas").style.visibility = "hidden";
 document.getElementById("page2").style.display= "none";
 
@@ -15,13 +15,15 @@ document.getElementById("contenedorTarjetas").style.visibility = "visible";
 console.log(data)
 
 
+
 //crear opciones para seleccionar el país
 function showOptionsSelect (){
   let team = [];
-  let selectPais = document.getElementById("selectBusquedaPais");
+  let selectPais = document.getElementById("selectSearchByTeam");
     data.athletes.forEach(atleta =>{
       if(team.indexOf(atleta["team"]) == -1){
           team.push(atleta["team"]);
+         
       }
 
 });
@@ -34,48 +36,53 @@ function showOptionsSelect (){
 } showOptionsSelect();
 
 
-function showAthletesByMedal() {
- let dataSearch = document.getElementById("txtAtleta").value;
- let firstMayus = dataSearch[0].toUpperCase()
+function showAthletesByMedal(result) {
+ let dataSearch = document.getElementById("selectSearchByMedal").value;
+ let firstMayus = dataSearch[0].toUpperCase();
  let otherLetters = dataSearch.slice(1)
  let typeMedal = firstMayus + otherLetters;
- 
-  table.innerHTML = "";
-  let result = filterData(data, "medal", typeMedal );
-  console.log(result);
-  let encabezado = document.createElement("td");
-  encabezado.style.textAlign = "center";
-  encabezado.innerHTML = `Athletes who won  ${typeMedal + " medal "+" : "+ result.length}`;
-  table.appendChild(encabezado);
+  //table.innerHTML = "";
 
-    let flag = 0;
-    let limit = 25;
-    let contenedor;
-    result.forEach((element) => {
-      if(flag == limit){
-        flag = 0;
-      }
-      if(flag == 0){
-        contenedor = document.createElement("div");
-        contenedor.setAttribute("name", "helloButton");
-        contenedor.style.display = "inline-block";
-        contenedor.style.margin = "10px";
+    result = filterData(data, "medal",  typeMedal );
+    console.log(result);
 
-      }
-      let fila = document.createElement("div");
-      let colum = document.createElement("td");
-      colum.innerHTML = element.name;
-      fila.appendChild(colum); 
-      contenedor.appendChild(fila);
-      table.appendChild(contenedor); 
-      flag++;
-    });
-};
+      let encabezado = document.createElement("div");
+      encabezado.style.textAlign = "center";
+      encabezado.innerHTML = `Athletes who won  ${ typeMedal + " medal "+" : "+ result.length}`;
+      table.appendChild(encabezado);
+        let flag = 0;
+        let limit = 25;
+        let contenedor;
+        result.forEach((element) => {
+          if(flag == limit){
+            flag = 0;
+          }
+          if(flag == 0){
+            contenedor = document.createElement("div");
+            contenedor.setAttribute("name", "helloButton");
+            contenedor.style.display = "inline-block";
+            contenedor.style.margin = "3vw";
+            contenedor.style.textAlign="center"
+    
+          }
+          let fila = document.createElement("div");
+          let colum = document.createElement("td");
+          colum.innerHTML = element.name;
+          fila.appendChild(colum);
+          contenedor.appendChild(fila);
+          table.appendChild(contenedor);
+          flag++;
 
+        });
+  };
 function showAthletesBySport(){
 
-  let selectSport = document.getElementById("selectBusquedaDeporte").value;
+  let selectSport = document.getElementById("selectSearchBySport").value;
   let resultSport= filterData(data, "sport", selectSport );
+  let encabezado = document.createElement("div");
+  encabezado.style.textAlign=("center")
+  encabezado.style.width=("90vw")
+  encabezado.innerHTML=`Athletes of${selectSport +" "+ resultSport.length}`;
   console.log(resultSport)
   let flag = 0;
   let limit = 25;
@@ -99,13 +106,46 @@ function showAthletesBySport(){
     table.appendChild(contenedor); 
     flag++;
   });
-}  
+};
+
+function showAthletesByTeam(){
+
+  let selectTeam = document.getElementById("selectSearchByTeam").value;
+  let resultTeam= filterData(data, "team", selectTeam );
+  let encabezado = document.createElement("div");
+  encabezado.style.textAlign=("center")
+  encabezado.style.width=("90vw")
+  encabezado.innerHTML=`Athletes of${selectTeam +" "+ resultTeam.length}`;
+  console.log(resultTeam)
+  let flag = 0;
+  let limit = 19;
+  let contenedor;
+  resultTeam.forEach((element) => {
+    if(flag == limit){
+      flag = 0;
+    }
+    if(flag == 0){
+      contenedor = document.createElement("div");
+      contenedor.setAttribute("name", "helloButton");
+      contenedor.style.display = "inline-block";
+      contenedor.style.padding = "4vw";
+
+    }
+    let fila = document.createElement("div");
+    let colum = document.createElement("td");
+    colum.innerHTML = element.name;
+    fila.appendChild(colum); 
+    contenedor.appendChild(fila);
+    table.appendChild(contenedor); 
+    flag++;
+  });
+};
 
 
 //SHOW OPTION DEPORTE
 function showOptionsSelectSport(){
   let sport = [];
-  let selectSport = document.getElementById("selectBusquedaDeporte");
+  let selectSport = document.getElementById("selectSearchBySport");
     data.athletes.forEach(deporte =>{
       if(sport.indexOf(deporte["sport"]) == -1){
           sport.push(deporte["sport"]);
@@ -118,7 +158,7 @@ function showOptionsSelectSport(){
     selectSport.appendChild(sports);
   });
 };
-showOptionsSelectSport ()
+showOptionsSelectSport()
 
 
 //CAMBIAR DE PANTALLA
@@ -136,7 +176,20 @@ function backPage1(){
     document.getElementById("page2").style.display = "none";
 }
 
+//REFRESH SEARCH
+
+function refresh(){
+  document.getElementById("selectBusquedaPais").value = "";
+  document.getElementById("selectBusquedaPais").value = "";
+
+
+
+
+}
+
 btn1.addEventListener("click", shangePage);
 btnInicio.addEventListener("click", backPage1)
-document.getElementById("btnBuscar").addEventListener("click", showAthletesByMedal, showAthletesBySport )
-;
+document.getElementById("btnBuscar").addEventListener("click", showAthletesByMedal);
+document.getElementById("btnBuscar").addEventListener("click", showAthletesBySport );
+document.getElementById("btnBuscar").addEventListener("click", showAthletesByTeam );
+buttonRefresh.addEventListener("click", shangePage);
