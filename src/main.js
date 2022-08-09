ce6ebd70d6f3800abe2a507c9bdb0e9886f35c75
Tania1295 <108893226+Tania1295@users.noreply.github.com>
2022-08-09 13:17:24 -0500

    Modificación JS con respecto a función de visualizar personajes con botones, se incluye estilo para quitar el evento a una imagen, se incluye en el HTML data-character para enlazarlo con el botón.

--- src/main.js
+++ src/main.js
@@ -1,22 +1,31 @@
 import { example } from './data.js';
 import data from './data/harrypotter/data.js';
 
-document.getElementById("btnHarryPotter").addEventListener("click", showHarryPotter)
-function showHarryPotter(){
-const result = data.characters.filter(personage => personage.name =='Harry Potter')
-//console.log(result)
-alert(`Nombre: ${result[0].name} \n Fecha de Nacimiento: ${result[0].birth} \n Casa de Hogwarts: ${result[0].house} \n Tipo de Mago: ${result[0].species} `)
+document.getElementById("btnHarryPotter").addEventListener("click", showCharacters)
+document.getElementById("btnHermioneGranger").addEventListener("click", showCharacters)
+document.getElementById("btnRonaldWeasley").addEventListener("click", showCharacters)
+document.getElementById("btnAlbusDumbledore").addEventListener("click", showCharacters)
+
+function showCharacters(e) {
+    const character = e.target.dataset.character
+    console.log(character)
+    const result = data.characters.filter(personage => personage.name == character)
+    alert(`Nombre: ${result[0].name} \n Fecha de Nacimiento: ${result[0].birth} \n Casa de Hogwarts: ${result[0].house} \n Tipo de Mago: ${result[0].species} `)
 }
-const result1 = data.characters.filter(personage => personage.name =='Hermione Granger')
-photoHermioneGranger.innerHTML += `<li>${result1[0].name} - ${result1[0].house} </li>`
 
 
-const personajes = data.characters;
+
+
+//for(const [key, value] of Object.entries(data.characters)){
+ //   console.log(value)
+//}
+
+
+/* const personajes = data.characters;
 const imagenHarryPotter = document.getElementById("imagen_harry_potter");
 const personajeSeleccionado = data.characters.filter(personaje => personaje.name === "Harry Potter");
 console.log(personajeSeleccionado[0].name)
 imagenHarryPotter.innerHTML += `<li>${personajeSeleccionado[0].name} - ${personajeSeleccionado[0].house} </li>`
-
 data.characters.forEach(personaje => {
 })
 
@@ -29,10 +38,4 @@ photoAlbusDumbledore.innerHTML += `<li>${result3[0].name} - ${result3[0].house} 
 
 //const harrypotter = data.characters.find(harryPotter => harryPotter.name === "Harry Potter");
 const ron = data.characters.find(ron => ron.name === "Ronald Weasley");
-console.log (harrypotter, ron)
-
-f//or(const [key, value] of Object.entries(data.characters)){
- //   console.log(value)
-//}
-
-
+console.log (harrypotter, ron) */
