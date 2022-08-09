da346048c80f22feb755f8e69fce1950842d8761
Tania1295 <108893226+Tania1295@users.noreply.github.com>
2022-08-08 21:05:38 -0500

    Cambios en CSS, HTML y JS de función visualizar historia de usuaria 1

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 import { example } from './data.js';
 import data from './data/harrypotter/data.js';
 
-console.log(example, data.characters);
-
-//const personajes = data.characters
-
-//personajes.forEach(personaje => console.log(personaje))
-
-//const harrypotter = data.characters.find(harryPotter => harryPotter.name === "Harry Potter");
-//const ron = data.characters.find(ron => ron.name === "Ronald Weasley");
-//console.log (harrypotter, ron)
-
-for(const [key, value] of Object.entries(data.characters)){
-    console.log(value)
+document.getElementById("btnHarryPotter").addEventListener("click", showHarryPotter)
+function showHarryPotter(){
+const result = data.characters.filter(personage => personage.name =='Harry Potter')
+//console.log(result)
+alert(`${result[0].name} - ${result[0].house}`)
 }
+const result1 = data.characters.filter(personage => personage.name =='Hermione Granger')
+photoHermioneGranger.innerHTML += `<li>${result1[0].name} - ${result1[0].house} </li>`
+
+const result2 = data.characters.filter(personage => personage.name =='Ronald Weasley')
+photoRonWeasley.innerHTML += `<li>${result2[0].name} - ${result2[0].house} </li>`
+
+const result3 = data.characters.filter(personage => personage.name =='Albus Dumbledore')
+photoAlbusDumbledore.innerHTML += `<li>${result3[0].name} - ${result3[0].house} </li>`
+
