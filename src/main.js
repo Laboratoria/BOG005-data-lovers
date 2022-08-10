02e272613d98ff4c511e48c74013fc556ea50d43
Tania1295 <108893226+Tania1295@users.noreply.github.com>
2022-08-10 13:16:26 -0500

    Cambios OH Genesis

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import data from './data/harrypotter/data.js';
+console.log(data.characters)
 
-document.getElementById("btnHarryPotter").addEventListener("click", showCharacters)
+/* document.getElementById("btnHarryPotter").addEventListener("click", showCharacters)
 document.getElementById("btnHermioneGranger").addEventListener("click", showCharacters)
 document.getElementById("btnRonaldWeasley").addEventListener("click", showCharacters)
 document.getElementById("btnAlbusDumbledore").addEventListener("click", showCharacters)
@@ -9,12 +10,30 @@ function showCharacters(e) {
     const character = e.target.dataset.character
     const result = data.characters.filter(personage => personage.name == character)
     alert(`Nombre: ${result[0].name} \n Fecha de Nacimiento: ${result[0].birth} \n Casa de Hogwarts: ${result[0].house} \n Tipo de Mago: ${result[0].species} `)
-}
+} */
+
+//const allCharacters = data.characters
+const allCharacters = data.characters.slice(0,20)
+const content = document.querySelector("#showCharacters")
+
+function showAllCharacters (){
+    allCharacters.forEach(element =>
+    //content.innerHTML+=element.name
+    //Template string
+    content.innerHTML += `<section class="card">
+    <img src="./img/logo_hp2.png" alt="">
+    <h3>${element.name}</h3>
+    <button> Ver +
+  </button>
+  </section>`
+  //interpolacion de variables 
+)}
+showAllCharacters()
+
 
 //for(const [key, value] of Object.entries(data.characters)){
  //   console.log(value)
 //}
-
 
 /* const personajes = data.characters;
 const imagenHarryPotter = document.getElementById("imagen_harry_potter");
