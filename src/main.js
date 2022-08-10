65c651fd326fa2f6fcda4abe7442efd05e7f4637
yenymag22 <yenyespinosa25@gmail.com>
2022-08-10 12:18:29 -0500

    visualizar imagenes y nombres pokemon

--- src/main.js
+++ src/main.js
@@ -1,6 +1,40 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+//import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//nombres de cada pokemon//
+let pokemon = Object.values(data.pokemon);
+
+function visualizardata(datos) {
+    for (let i = 0; i < pokemon.length; i++) {
+        let namepokemon = pokemon[i].name;
+        let imagenpokemon = pokemon[i].img;
+        let contenedor = document.querySelector(".contenedor")
+        let infopokemon = document.createElement("div");
+        contenedor.appendChild(infopokemon)
+        infopokemon.innerHTML =
+            `
+<img src = "${imagenpokemon}">
+            <p> ${namepokemon}</p>
+
+             `
+    }
+
+}
+
+
+window.addEventListener("load", visualizardata(pokemon))
+
+//ESTO ES UN OBJETO  const data = {
+//     pokemon:{
+//         nombre:pikachu,
+//         edad:"3 años"
+//     },
+
+// }
+
+// function suma(a,b) {
+//     return a+b
+// }
+//suma(3,4)
+
+//[ ]gfgfd
