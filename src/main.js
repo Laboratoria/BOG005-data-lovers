f011623c0683656fbf7a81512558e8a78003f69b
Tania1295 <108893226+Tania1295@users.noreply.github.com>
2022-08-11 14:22:51 -0500

    Cambios en js

--- src/main.js
+++ src/main.js
@@ -6,13 +6,13 @@ const content = document.querySelector("#showCharacters");
 function showAllCharacters() {
   allCharacters.forEach(
     (element) =>
-      //content.innerHTML+=element.name
-      //Template string
-      (content.innerHTML += `<section class="card">
+    //content.innerHTML+=element.name
+    //Template string
+    (content.innerHTML += `<section class="card">
     <li><img class="wand"src="Img/varita.png" alt="">
     <br>
     <h3>${element.name}</h3>
-    <button id="showMore" class="buttonShow"> Ver +
+    <button id=${element.name} class="buttonShow"> Ver +
   </button></li>
   </section>`)
     //interpolacion de variables
@@ -20,47 +20,17 @@ function showAllCharacters() {
 }
 showAllCharacters();
 
-document
-  .getElementById("showMore")
-  .addEventListener("click", showCharactersInfo);
-const information = document.querySelector("#btnCharacters");
-function showCharactersInfo() {
-  for (let i = 0; i < allCharacters.length; i++) {
-    information.innerHTML += `<section class="information">
-  Nombre: ${allCharacters[i].name} \n Fecha de Nacimiento: ${allCharacters[i].birth} \n Casa de Hogwarts: ${allCharacters[i].house} \n Tipo de Mago: ${allCharacters[i].species}
-  </section>`;
-  }
-}
-//for(const [key, value] of Object.entries(data.characters)){
-//   console.log(value)
-//}
+const btnVer = document.querySelectorAll(".buttonShow")
+const information = document.querySelector("#informationCharacters")
+btnVer.forEach((button) => {
+  button.addEventListener("click", (evento) => {
+  //console.log(evento.target.id)
 
-/* const personajes = data.characters;
-const imagenHarryPotter = document.getElementById("imagen_harry_potter");
-const personajeSeleccionado = data.characters.filter(personaje => personaje.name === "Harry Potter");
-console.log(personajeSeleccionado[0].name)
-imagenHarryPotter.innerHTML += `<li>${personajeSeleccionado[0].name} - ${personajeSeleccionado[0].house} </li>`
-data.characters.forEach(personaje => {
+    (information.innerHTML += `<div class="information">
+Nombre: ${button.name} \n
+Fecha de Nacimiento: ${allCharacters[0].birth}
+\n Casa de Hogwarts: ${allCharacters[0].house}
+\n Tipo de Mago: ${allCharacters[0].species}
+</div>`)
+  })
 })
-
-const result2 = data.characters.filter(personage => personage.name =='Ronald Weasley')
-photoRonWeasley.innerHTML += `<li>${result2[0].name} - ${result2[0].house} </li>`
-
-const result3 = data.characters.filter(personage => personage.name =='Albus Dumbledore')
-photoAlbusDumbledore.innerHTML += `<li>${result3[0].name} - ${result3[0].house} </li>`
-
-
-//const harrypotter = data.characters.find(harryPotter => harryPotter.name === "Harry Potter");
-const ron = data.characters.find(ron => ron.name === "Ronald Weasley");
-console.log (harrypotter, ron) */
-
-/* document.getElementById("btnHarryPotter").addEventListener("click", showCharacters)
-document.getElementById("btnHermioneGranger").addEventListener("click", showCharacters)
-document.getElementById("btnRonaldWeasley").addEventListener("click", showCharacters)
-document.getElementById("btnAlbusDumbledore").addEventListener("click", showCharacters)
-
-function showCharacters(e) {
-    const character = e.target.dataset.character
-    const result = data.characters.filter(personage => personage.name == character)
-    alert(`Nombre: ${result[0].name} \n Fecha de Nacimiento: ${result[0].birth} \n Casa de Hogwarts: ${result[0].house} \n Tipo de Mago: ${result[0].species} `)
-} */
