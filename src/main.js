08917c963e64c0a8692f12c04a421df68a67152f
Tania1295 <108893226+Tania1295@users.noreply.github.com>
2022-08-12 13:26:37 -0500

    Cambios despupes OH 12 de agosto

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
 import data from "./data/harrypotter/data.js";
 
-const allCharacters = data.characters.slice(0, 20);
+console.log(data.funFacts)
+
+const allCharacters = data.characters.slice(0, 21);
 const content = document.querySelector("#showCharacters");
 
 function showAllCharacters() {
   allCharacters.forEach(
-    (element) =>
+    (element, i) =>
     //content.innerHTML+=element.name
     //Template string
     (content.innerHTML += `<section class="card">
-    <li><img class="wand"src="Img/varita.png" alt="">
+    <ul><li><img class="wand"src="Img/harry_potter_2-.png" alt="">
     <br>
     <h3>${element.name}</h3>
-    <button id=${element.name} class="buttonShow"> Ver +
-  </button></li>
+    <button data-id=${i} class="buttonShow"> Ver +
+  </button></li></ul>
   </section>`)
     //interpolacion de variables
   );
@@ -23,14 +25,15 @@ showAllCharacters();
 const btnVer = document.querySelectorAll(".buttonShow")
 const information = document.querySelector("#informationCharacters")
 btnVer.forEach((button) => {
-  button.addEventListener("click", (evento) => {
-  //console.log(evento.target.id)
-
-    (information.innerHTML += `<div class="information">
-Nombre: ${button.name} \n
-Fecha de Nacimiento: ${allCharacters[0].birth}
-\n Casa de Hogwarts: ${allCharacters[0].house}
-\n Tipo de Mago: ${allCharacters[0].species}
-</div>`)
+  button.addEventListener("click", (event) => {
+    const i = event.target.dataset.id
+    information.innerHTML += `<div>
+  Nombre: ${allCharacters[i].name ? allCharacters[i].name : "Sin información"} \n
+  Fecha de Nacimiento: ${allCharacters[i].birth ? allCharacters[i].birth : "Sin información"}
+  \n Casa de Hogwarts: ${allCharacters[i].house ? allCharacters[i].house : "Sin información"}
+  \n Tipo de Mago: ${allCharacters[i].species ? allCharacters[i].species : "Sin información"}
+  </div>`
   })
 })
+
+
