43b38021855a6aa07dd6fea4b73051f5ff31d28a
Tania1295 <108893226+Tania1295@users.noreply.github.com>
2022-08-16 15:42:42 -0500

    Inclusión de secciones en HTML, creación función btn mostrar en JS

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import data from "./data/harrypotter/data.js";
 
+//Mostrar sección a través de botón
+
+let btnShowSectionCharacters = document.getElementById("nav-characters")
+btnShowSectionCharacters.addEventListener("click", changeBtnStyle)
+
+function changeBtnStyle(){
+  document.getElementById("showCharacters").style.display = "none"
+  document.getElementById("showCharacters").style.display = "flex"
+}
+
+//Función visualizar personajes
 const allCharacters = data.characters.slice(0, 21);
 const content = document.querySelector("#showCharacters");
 
@@ -7,13 +18,13 @@ function showAllCharacters() {
   allCharacters.forEach(
     (element, i) =>
     //Template string
-    (content.insertAdjacentHTML("beforeend", `<section class="card">
+    (content.innerHTML +=`<section class="card">
     <ul><img class="wand"src="Img/icons8-harry.png" alt="Imagen generica para personajes">
     <br>
     <h3 class="name-characters">${element.name}</h3>
     <button data-id=${i} class="btn buttonShow"> Ver +
   </button></ul>
-  </section>`))
+  </section>`)
     //Interpolacion de variables
   );
 }
@@ -24,13 +35,12 @@ const information = document.querySelector("#informationCharacters")
 btnVer.forEach((button) => {
   button.addEventListener("click", (event) => {
     const i = event.target.dataset.id
-    information.insertAdjacentHTML("beforeend",
-  `<div class="information">
+    information.innerHTML = `<div class="information">
   <strong>Nombre:</strong> ${allCharacters[i].name ? allCharacters[i].name : "Sin información"}
   <br> <strong>Fecha de Nacimiento:</strong> ${allCharacters[i].birth ? allCharacters[i].birth : "Sin información"}
   <br> <strong>Casa de Hogwarts:</strong> ${allCharacters[i].house ? allCharacters[i].house : "Sin información"}
   <br> <strong>Tipo de Mago:</strong> ${allCharacters[i].species ? allCharacters[i].species : "Sin información"}
-  </div>`)
+  </div>`
   })
 })
 
