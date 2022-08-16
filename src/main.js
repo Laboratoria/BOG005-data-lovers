5c01cd7d4d8f412ddfb2ae6ee7dcf28d534dbb55
Tania1295 <108893226+Tania1295@users.noreply.github.com>
2022-08-15 21:46:49 -0500

    Cambios en CSS y cambio de class en JS y organización de clases en el archivo CSS

--- src/main.js
+++ src/main.js
@@ -6,16 +6,15 @@ const content = document.querySelector("#showCharacters");
 function showAllCharacters() {
   allCharacters.forEach(
     (element, i) =>
-    //content.innerHTML+=element.name
     //Template string
     (content.insertAdjacentHTML("beforeend", `<section class="card">
-    <ul><img class="wand"src="Img/harry_potter_2-.png" alt="Imagen generica para personajes">
+    <ul><img class="wand"src="Img/icons8-harry.png" alt="Imagen generica para personajes">
     <br>
-    <h3>${element.name}</h3>
+    <h3 class="name-characters">${element.name}</h3>
     <button data-id=${i} class="btn buttonShow"> Ver +
   </button></ul>
   </section>`))
-    //interpolacion de variables
+    //Interpolacion de variables
   );
 }
 showAllCharacters();
@@ -25,12 +24,13 @@ const information = document.querySelector("#informationCharacters")
 btnVer.forEach((button) => {
   button.addEventListener("click", (event) => {
     const i = event.target.dataset.id
-    information.innerHTML += `<div>
-  Nombre: ${allCharacters[i].name ? allCharacters[i].name : "Sin información"} \n
-  Fecha de Nacimiento: ${allCharacters[i].birth ? allCharacters[i].birth : "Sin información"}
-  \n Casa de Hogwarts: ${allCharacters[i].house ? allCharacters[i].house : "Sin información"}
-  \n Tipo de Mago: ${allCharacters[i].species ? allCharacters[i].species : "Sin información"}
-  </div>`
+    information.insertAdjacentHTML("beforeend",
+  `<div class="information">
+  <strong>Nombre:</strong> ${allCharacters[i].name ? allCharacters[i].name : "Sin información"}
+  <br> <strong>Fecha de Nacimiento:</strong> ${allCharacters[i].birth ? allCharacters[i].birth : "Sin información"}
+  <br> <strong>Casa de Hogwarts:</strong> ${allCharacters[i].house ? allCharacters[i].house : "Sin información"}
+  <br> <strong>Tipo de Mago:</strong> ${allCharacters[i].species ? allCharacters[i].species : "Sin información"}
+  </div>`)
   })
 })
 
