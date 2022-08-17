f258b8026a63bde94d439f208dd49dd9fb7356d2
Monik586 <monik.rodriguez586@gmail.com>
2022-08-17 12:31:47 -0500

    cambis al css y html

--- src/data.js
+++ src/data.js
@@ -1,9 +1,27 @@
-// estas funciones son de ejemplo
+// // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+// export const example = () => {
+//   return 'example';
+// };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetchData()
+  })
+ 
+  export const fetchData = async () => {
+     try {
+       const res = await fetch('athletes.json')
+      const data = await res.json()
+       console.log(data)
+     } catch (error) {
+     console.log(error)
+     
+ 
+ }
+  }
+ 
