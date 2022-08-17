eaa0809f079f2a5f42bac11022e364aef7b1298a
Katherine Cuartas Cardona <cuartascardonak@gmail.com>
2022-08-17 17:32:19 -0500

    ensayo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+// import { res } from './data.js';
+import data from './data/athletes/athletes.js';
+//import data from './data/athletes/athletes.json';
+// // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(data);
