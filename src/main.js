93e4aead2f0993f2312f3fb1d216d5e1c3cbc520
Patricia-Hernanandez <nenitagarzon90@gmail.com>
2022-08-22 08:17:27 -0500

    primeros cambios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-/*import { example } from './data.js';
+import POTTER from './data/harrypotter/harrypotter.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);*/
-import { filterGryffindor, filterSlytherin, filterRavenclaw, filterHufflepuff } from './data.js'
-import {POTTER} from './data/potter/potter.js'; //documento data
+console.log(example, data);
