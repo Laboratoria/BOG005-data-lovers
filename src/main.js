3f467bd00081af64dbcf98cf7541d15665e78f9e
Patricia-Hernanandez <nenitagarzon90@gmail.com>
2022-08-22 13:13:18 -0500

    cambios paginal menu

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import POTTER from './data/harrypotter/harrypotter.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
+// import data from harrypotter
+import {
+    filtrarPersonajes,
+    filtrarCasas,
+    filtrarOrdenAlfabetico,
+    buscarPorNombre,
+}from './data.js';
 console.log(example, data);
