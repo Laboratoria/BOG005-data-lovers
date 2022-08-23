a0285be523bb676aab96fbf8b36bda20ec2a3dba
Tania1295 <108893226+Tania1295@users.noreply.github.com>
2022-08-23 14:48:52 -0500

    Cambios en el menu responsive

--- src/main.js
+++ src/main.js
@@ -9,6 +9,15 @@ function changeBtnStyle () {
   document.getElementById('showCharacters').style.display = 'block'
 }
 
+// Menu
+
+const toggleMenuElement = document.getElementById('toggle-menu')
+const mainMenuElement = document.getElementById('main-menu')
+
+toggleMenuElement.addEventListener('click', () => {
+  mainMenuElement.classList.toggle('main-menu--show')
+})
+
 // Función visualizar personajes
 const allCharacters = data.characters.slice(0, 450)
 const content = document.querySelector('#showCharacters')
