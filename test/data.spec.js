ccbab9aecb71780d2e3bb72c6da292a7ad2f01ed
Mariana Santos <mjsc1902@gmail.com>
2022-08-25 10:33:59 -0500

    ordenamos de la A-Z todos nuestros campos del menú, tambien tenemos la base de un test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,28 @@
-import { example, anotherExample } from '../src/data.js';
+// import { example, anotherExample } from '../src/data.js';
 
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+import { sortByNameOrTitle } from "../src/data.js";
+describe("ssortByNameOrTitleort", () => {
+  it("is a function", () => {
+    expect(typeof sortByNameOrTitle).toBe("function");
   });
 });
 
+// describe('example', () => {
+//   it('is a function', () => {
+//     expect(typeof example).toBe('function');
+//   });
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
+//   it('returns `example`', () => {
+//     expect(example()).toBe('example');
+//   });
+// });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
+// describe('anotherExample', () => {
+//   it('is a function', () => {
+//     expect(typeof anotherExample).toBe('function');
+//   });
+
+//   it('returns `anotherExample`', () => {
+//     expect(anotherExample()).toBe('OMG');
+//   });
+// });
