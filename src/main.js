25b38773916bd6f5ff4d531dd1879e6fa3c65a87
LauraCardenas <laurac1996@outlook.es>
2022-08-25 22:55:48 -0500

    paginacion

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ console.log(athletes.athletes);
 makeCard(athletes.athletes);
 let posicionInicial = 0;
 let posicionFinal = 52;
+
 function pagination(direction) {
   if (direction === "rigth") {
     posicionInicial += 52;
@@ -14,6 +15,7 @@ function pagination(direction) {
     posicionInicial -= 52;
     posicionFinal -= 52;
   }
+  console.log(pagination);
   makeCard(athletes);
 }
 function makeCard() {
@@ -29,20 +31,18 @@ function makeCard() {
           </section>
         <section class="cardBack">
           <h1 class = "nameBack"> ${athletes[i].name} </h1> 
-          <p> Pais: ${athletes.team}</p>
-          <p> Pais: ${athletes.team}</p>
-          <p>Medalla: ${athletes.medal}</p>
-          <p>Deporte: ${athletes.sport} </p>
-          <p>Altura: ${athletes.height}</p>
-          <p>Peso: ${athletes.weight}</p>
+          <p> Pais: ${athletes[i].team}</p>
+          <p> Pais: ${athletes[i].team}</p>
+          <p>Medalla: ${athletes[i].medal}</p>
+          <p>Deporte: ${athletes[i].sport} </p>
+          <p>Altura: ${athletes[i].height}</p>
+          <p>Peso: ${athletes[i].weight}</p>
         </section> 
       </section>
     </section>`;
   }
 
-  //       <div class="bodyCardFront">
-  //     </div>
-  //
+  //       <div class="bodyCardFront"></div>
   //       <div class="bodyCardBack"></div>
   container.innerHTML = finalHtml;
 }
