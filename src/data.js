0ba56baaadefd88537eab6202c994a15259b9b8b
Patricia-Hernanandez <nenitagarzon90@gmail.com>
2022-08-30 12:27:36 -0500

    ordenamiento arbol del DOM, codigo comentado e inicio de filtros

--- src/data.js
+++ src/data.js
@@ -1,9 +1,13 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+export const alphabeticalOrder = (filteredData, sortBy) => {
+  switch (sortBy) {
+    case "za":
+      return filteredData.sort(function (a, b) {
+        return b.name.localeCompare(a.name);
+      });
+    case "az":
+    default:
+      return filteredData.sort(function (a, b) {
+        return a.name.localeCompare(b.name);
+      });
+  }
 };
-
-export const filtrarPersonajes= (datosHarry, string) => {
-  const nuevoArray = datosHarry.filter((obj.))
-}
--- src/main.js
+++ src/main.js
@@ -1,47 +1,111 @@
 import data from './data/harrypotter/data.js'; //importamos la data.
-
-console.log('example', data);
-
 const books = data.books; //declaramos una constante de la data
-const characters = data.characters
-console.log(books, characters);
+const characters = data.characters;
+//importamos filtros
+/*import{
+  filterGender,
+  HogwartsHouses,
+  alphabeticalOrder,
+}from './data.js';*/
 
+/*let selectedHouse = "";*/
+/*let sortBy = "";*/
 
+//selectores del DOM
+const welcome = document.getElementById("welcome");
+const page1 = document.getElementById("pag1");
+const page2 = document.getElementById("pag2");
+const HomeBack = document.getElementById("homeBack");
+let containerCard = document.querySelector(".containerCard");
+const AlphabeticalOrder = document.querySelector("alphabeticalOrder");
+/*let containerCard = document.querySelector(".containerCard");*/
+
+//funcion boton inicio
+
+welcome.addEventListener("click", changePage)
+function changePage() {
+  page1.style.display = "none";
+  page2.style.display = "block";
+}
+//funcion boton regresar inicio
+HomeBack.addEventListener("click",changePage2)
+function changePage2(){
+  page1.style.display = "block";
+  page2.style.display = "none";
+}
+
+//funcion creacion de tarjetas y pintar data dentro de ellas
 function card(characters) {
-    
-    let containerCard = document.querySelector(".containerCard");
-    let totalCard=''
+
+  
+  let totalCard = ''
   characters.forEach((element) => {
     totalCard += `    
-   
-    <article class="containerFather">
-    <nav class="card">
-      <nav class="cardFront" style="background-image:url(imgs/escudo4.jpeg)"  alt=""> 
-        <nav class="bodyCardFront">
-        </nav>
-      </nav>
-      <nav class="cardBack" style="background-image:url(imgs/background.jpeg)">
-        <nav class="bodyCardBack">
-          <p> ${element.name} </p>
-          <p> ${element.gender}</p>
-          <p> ${element.birth}</p>
-          <p> ${element.species}</p>
-          
-        </nav>
+ 
+  <article class="containerFather">
+  <nav class="card">
+    <nav class="cardFront" style="background-image:url(imgs/escudo4.jpeg)"  alt=""> 
+      <nav class="bodyCardFront">
       </nav>
     </nav>
-  </article>`
-  
+    <nav class="cardBack" style="background-image:url(imgs/background.jpeg)">
+      <nav class="bodyCardBack">
+        <p> ${element.name} </p>
+        <p> ${element.gender}</p>
+        <p> ${element.birth}</p>
+        <p> ${element.species}</p>
+        
+      </nav>
+    </nav>
+  </nav>
+</article>`
+
   });
-  containerCard.innerHTML=totalCard
+  containerCard.innerHTML = totalCard
+  console.log(totalCard)
 }
 card(characters)
 
-
-let list = characters.filter(function(personajes){
-  return personajes.ancestry;
-})
-  
-  
+AlphabeticalOrder.addEventListener("change", function (event) {
+  sortBy = event.currentTarget.value;
+  renderFilteredData();
 });
+
+
+
+
+//funcion de filtrado
+
+/*let houses = characters.filter(function (HogwartsHouses) {
+  return HogwartsHouses.house === "Slytherin";
+
+})
+console.log(houses);
+
+let houses1 = characters.filter(function (HogwartsHouses) {
+  return HogwartsHouses.house === "Ravenclaw";
+})
+console.log(houses1);
+
+let houses2 = characters.filter(function (HogwartsHouses) {
+  return HogwartsHouses.house === "Gryffindor";
+})
+console.log(houses2);
+
+
+let houses3 = characters.filter(function (HogwartsHouses) {
+  return HogwartsHouses.house === "Hufflepuff";
+})
+console.log(houses3);
+
+let list1 = characters.filter(function (characters) {
+  return characters.gender === "Male";
+})
+console.log(list1);
+
+let list2 = characters.filter(function (characters) {
+  return characters.gender === "Female";
+})
+console.log(list2);*/
+
 
