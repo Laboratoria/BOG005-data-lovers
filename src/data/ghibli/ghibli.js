2ad825e37162093ee232ffa505ce09e0446ba458
Andrea Zapata <95andreazapata@gmail.com>
2022-09-01 19:40:58 -0500

    Tests for filter function

--- src/data/ghibli/ghibli.js
+++ src/data/ghibli/ghibli.js
@@ -1605,7 +1605,7 @@ export default {
       description:
         "Something bizarre has come over the land. The kingdom is deteriorating. People are beginning to act strange... What's even more strange is that people are beginning to see dragons, which shouldn't enter the world of humans. Due to all these bizarre events, Ged, a wandering wizard, is investigating the cause. During his journey, he meets Prince Arren, a young distraught teenage boy. While Arren may look like a shy young teen, he has a severe dark side, which grants him strength, hatred, ruthlessness and has no mercy, especially when it comes to protecting Teru. For the witch Kumo this is a perfect opportunity. She can use the boy's 'fears' against the very one who would help him, Ged.",
       director: "Gorō Miyazaki",
-      producer: "Toshio Suzuki",
+      producer: "",
       poster:
         "https://static.wikia.nocookie.net/studio-ghibli/images/0/09/%C3%96v%C3%A4rlden.jpg",
       release_date: "2006",
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,19 +1,47 @@
-import {sortByNameOrTitle} from "../src/data.js";
+import { filterByDirector, sortByNameOrTitle } from "../src/data.js";
 
 // Test para función de ordenar A-Z
 
 describe("sortByNameOrTitle", () => {
-  it("is a function", () => {
+  it("Es una función", () => {
     expect(typeof sortByNameOrTitle).toBe("function");
+  });
+  it("Filtra en el orden que corresponde", () => {
+    let data = [{ title: "a" }, { title: "b" }, { title: "c" }, { title: "z" }];
   });
 });
 
-/*it('Orders from A to Z', () => {
+// Test para función filtrar por director
 
-  let moviesOrder = sortByNameOrTitle(Object.values(moviesSample), 'Z-A');
+describe("filterByDirector", () => {
+  it("Filtra por Isao Takahata", () => {
+    let data = [
+      { director: "Isao Takahata" },
+      { director: "Hayao Miyazaki" },
+      { director: "Hiroyuri Morita" },
+    ];
+    let director = "Isao Takahata";
+    let expectedData = [{ director: "Isao Takahata" }];
+    let realResult = filterByDirector(data, director);
 
-  expect(moviesOrder[0]).toEqual(moviesSample."CastleInTheSky")
-  expect(moviesOrder[3]).toEqual(moviesSample."HowlsMovingCastle")
+    expect(realResult).toStrictEqual(expectedData);
+  });
 
-})*/
+  it("Filtra por Hayao Miyazaki", () => {
+    let data = [
+      { director: "Gorō Miyazaki" },
+      { director: "Toshio Suzuki" },
+      { director: "Hayao Miyazaki" },
+    ];
+    let director = "Hayao Miyazaki";
+    let expectedData = [{ director: "Hayao Miyazaki" }];
+    let realResult = filterByDirector(data, director);
 
+    expect(realResult).toStrictEqual(expectedData);
+  });
+});
+
+it("Retorna una lista de objetos", () => {
+  const lista = filterByDirector(data, []);
+  expect(lista).toEqual(["something"]);
+});
