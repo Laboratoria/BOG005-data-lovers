41a597de39abed57c83f4b1b20c8f9de52d216f6
LauraCardenas <laurac1996@outlook.es>
2022-09-12 08:55:36 -0500

    Con data cortada

--- src/data.js
+++ src/data.js
@@ -1,13 +1,14 @@
 // estas funciones son de ejemplo
 
+export const countryFilter = (deportes, opcion) => {
+  console.log(deportes, opcion);
+  const paisFiltrado = deportes.filter((Element) => Element.team === opcion);
 
-export const countryFilter= (deportes , opcion) => { 
-  const paisFiltrado = deportes.filter(Element => Element.team === opcion)
   return paisFiltrado;
 };
 
 //filtro guia test
-/*export const countryFilter= (deportes , opcion) => { 
-  const paisFiltrado = deportes.filter(Element => Element.team === opcion)
+/*export const countryFilter = (deportes, opcion) => {
+  const paisFiltrado = deportes.filter((Element) => Element.team === opcion);
   return paisFiltrado;
 };*/
